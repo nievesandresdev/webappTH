046cbae5145d2460bb3b41b0cf07b390d597c6d4
Andres <andresdreamerf@gmail.com>
2024-02-07 09:21:00 -0400

    config

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,12 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    port: 80
+    port: 80,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true
+      }
+    }
   }
 });
