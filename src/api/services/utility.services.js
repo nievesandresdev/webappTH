575a7f117f408a26be698d9aa19909e1d41b423c
Andres <andresdreamerf@gmail.com>
2024-02-07 17:43:43 -0400

    fix

--- src/api/services/utility.services.js
+++ src/api/services/utility.services.js
@@ -4,5 +4,4 @@ import {
 } from '../config/apiRoute'
 
 export const getExpAndPlaceBySaearchApi = (data) => apiHttp('GET', `${utilityPath}/getExpAndPlaceBySaearch`, data, { showPreloader: false })
-// export const getPhoneCodesApi = () => apiHttp('GET', `${utilityPath}/getPhoneCodesApi`, [], { showPreloader: false })
-export const getPhoneCodesApi = () => `${utilityPath}/getPhoneCodesApi`;
+export const getPhoneCodesApi = () => apiHttp('GET', `${utilityPath}/getPhoneCodesApi`, [], { showPreloader: false })
