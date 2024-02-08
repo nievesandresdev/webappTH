9f1cc3de6605a4f5a87fbc111979337a60f2b679
Andres <andresdreamerf@gmail.com>
2024-02-08 17:24:40 -0400

    fix

--- src/stores/modules/locale.js
+++ src/stores/modules/locale.js
@@ -11,13 +11,15 @@ export const useLocaleStore = defineStore('locale', () => {
     // ACTIONS
     function $change (lg) {
         localStorage.setItem('locale', lg)
+        // console.log('changeAndReload',localStorage.getItem('locale'))
         i18n.global.locale.value = lg
         localeCurrent.value = lg
     }
 
     function $load () {
+        // console.log('i18n.global.locale')
         const urlParams = new URLSearchParams(window.location.search)
-        let locale = urlParams.get('lang') || localeCurrent.value
+        let locale = localStorage.getItem('locale') ?? urlParams.get('lang');
         locale = availableLocation.value.includes(locale) ? locale : 'es'
         $change(locale)
     }
