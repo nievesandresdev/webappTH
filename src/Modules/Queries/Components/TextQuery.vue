de12bd99311214ee39a62339611f880793aa6935
Andres <andresdreamerf@gmail.com>
2024-03-12 07:53:37 -0400

    x

--- src/Modules/Queries/Components/TextQuery.vue
+++ src/Modules/Queries/Components/TextQuery.vue
@@ -68,19 +68,19 @@ async function saveQuery(){
     console.log('params',params)
     let response = await queryStore.$saveResponse(params);
     console.log('saveQuery',response)
-    if(response){
-        emit('showFeedback',props.settings.pre_stay_thanks.es);
-        setTimeout(() => {
-            toast(t('query.textToast.sendQueryText'), {
-                toastClassName: "warning-toast",
-                bodyClassName: "warning-toast-body",
-                position: "top-right",
-                icon: false,
-                closeButton: false,  
-                hideProgressBar: true,
-            });
-        }, 1000);
-    }
+    // if(response){
+    //     emit('showFeedback',props.settings.pre_stay_thanks.es);
+    //     setTimeout(() => {
+    //         toast(t('query.textToast.sendQueryText'), {
+    //             toastClassName: "warning-toast",
+    //             bodyClassName: "warning-toast-body",
+    //             position: "top-right",
+    //             icon: false,
+    //             closeButton: false,  
+    //             hideProgressBar: true,
+    //         });
+    //     }, 1000);
+    // }
 }
 
 </script>
