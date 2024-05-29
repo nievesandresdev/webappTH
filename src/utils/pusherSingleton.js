aafef7c6b36f95b0e768232889010f243f99315e
Andres <andresdreamerf@gmail.com>
2024-05-29 08:56:22 -0400

    fix

--- src/utils/pusherSingleton.js
+++ src/utils/pusherSingleton.js
@@ -11,7 +11,7 @@ export function getPusherInstance() {
         // console.log("Creando instancia de pusher...");
         pusherInstance = new Pusher(process.env.VUE_APP_PUSHER_APP_KEY, {
             cluster: 'us2',
-            authEndpoint: `${process.env.VUE_APP_API_URL_BACKEND}/${utilsPath}/authPusher`,
+            authEndpoint: `${process.env.VUE_APP_API_URL_BACKEND_GENERAL}/${utilsPath}/authPusher`,
             auth: {
                 headers: {
                     'x-key-api': process.env.VUE_APP_X_KEY_API
