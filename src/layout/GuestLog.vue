781c930087d092a35520eb6b55c61be5505f1f1f
Andres <andresdreamerf@gmail.com>
2024-06-14 10:53:11 -0400

    fix modal

--- src/layout/GuestLog.vue
+++ src/layout/GuestLog.vue
@@ -1,72 +1,71 @@
 <template>
-    <Dialog :open="openModal" class="relative">
-    <!-- The backdrop, rendered as a fixed sibling to the panel container -->
-    <div class="fixed top-0 left-0 h-screen w-full bg-[#00000080] z-[2000]" aria-hidden="true" />
+    <div :open="openModal" class="relative">
+        <div class="fixed top-0 left-0 h-screen w-full bg-[#00000080] z-[2000]"></div>
 
-    <!-- Full-screen container to center the panel -->
-    <div 
-        class="fixed left-0 lg:top-0 bottom-0 lg:right-0 flex w-screen items-center justify-center z-[2500]" 
-        :class="openModal ? 'dialog-enter-active' : 'dialog-leave-active'"
-    >
-        <!-- The actual dialog panel -->
-        <DialogPanel class="w-full lg:max-w-[360px] bg-white rounded-t-[0.85rem] lg:rounded-b-[0.85rem]">
-            <div class="relative">
-                <h1 class="text-lg font-medium text-center leading-6 pt-4">
-                    {{ $utils.capitalize($t('guest.guestLog.title')) }}
-                </h1>
-                <div class="absolute top-3 right-4">
-                    <MiniLangDropdown v-model="form.language" />
+        <!-- Full-screen container to center the panel -->
+        <div 
+            class="fixed left-0 lg:top-0 bottom-0 lg:right-0 flex w-screen items-center justify-center z-[2500]" 
+            :class="openModal ? 'dialog-enter-active' : 'dialog-leave-active'"
+        >
+            <!-- The actual dialog panel -->
+            <div class="w-full lg:max-w-[360px] bg-white rounded-t-[0.85rem] lg:rounded-b-[0.85rem]">
+                <div class="relative">
+                    <h1 class="text-lg font-medium text-center leading-6 pt-4">
+                        {{ $utils.capitalize($t('guest.guestLog.title')) }}
+                    </h1>
+                    <div class="absolute top-3 right-4">
+                        <MiniLangDropdown v-model="form.language" />
+                    </div>
                 </div>
-            </div>
-            
-            <div class="">
-                <div class="mt-4 px-4">
-                    <THInputText
-                        :textLabel="$t('guest.guestLog.name.label')"
-                        :placeholder="$t('guest.guestLog.name.placeholder')"
-                        v-model="form.name"
-                        :customClasses="{
-                            'hborder-gray-400':!form.name,
-                            'hborder-alert-negative':errorsKey.includes('name'),
-                        }"
-                    />
+                
+                <div class="">
+                    <div class="mt-4 px-4">
+                        <THInputText
+                            :textLabel="$t('guest.guestLog.name.label')"
+                            :placeholder="$t('guest.guestLog.name.placeholder')"
+                            v-model="form.name"
+                            :customClasses="{
+                                'hborder-gray-400':!form.name,
+                                'hborder-alert-negative':errorsKey.includes('name'),
+                            }"
+                        />
+                    </div>
+                    <div class="mt-4 px-4" v-if="guestData?.name !== null">
+                        <THInputText
+                            :textLabel="$t('guest.guestLog.email.label')"
+                            :placeholder="$t('guest.guestLog.email.placeholder')"
+                            :type="'email'"
+                            v-model="form.email"
+                            @handleError="emailError = $event"
+                            :customClasses="{
+                                'hborder-gray-400':!form.email
+                            }"
+                            :textError="$t('guest.guestLog.email.error')"
+                        />
+                    </div>
                 </div>
-                <div class="mt-4 px-4" v-if="guestData?.name !== null">
-                    <THInputText
-                        :textLabel="$t('guest.guestLog.email.label')"
-                        :placeholder="$t('guest.guestLog.email.placeholder')"
-                        :type="'email'"
-                        v-model="form.email"
-                        @handleError="emailError = $event"
-                        :customClasses="{
-                            'hborder-gray-400':!form.email
-                        }"
-                        :textError="$t('guest.guestLog.email.error')"
-                    />
+                <div class="mt-6 text-right p-4 border-t">
+                    <button 
+                        type="submit"
+                        class="hbtn-cta py-3 px-4 text-sm leading-4"
+                        :class="{'cta-disabled':!valid || processingForm}"
+                        :disabled="!valid || processingForm"
+                        @click="submitForm" 
+                    >
+                        {{ processingForm ? $t('guest.guestLog.processing')+'...' : $t('guest.guestLog.button') }}
+                    </button>
                 </div>
+                
             </div>
-            <div class="mt-6 text-right p-4 border-t">
-                <button 
-                    type="submit"
-                    class="hbtn-cta py-3 px-4 text-sm leading-4"
-                    :class="{'cta-disabled':!valid || processingForm}"
-                    :disabled="!valid || processingForm"
-                    @click="submitForm" 
-                >
-                    {{ processingForm ? $t('guest.guestLog.processing')+'...' : $t('guest.guestLog.button') }}
-                </button>
-            </div>
-            
-        </DialogPanel>
+        </div>
     </div>
-    </Dialog>
 </template>
 
 <script setup>
 import { onMounted, reactive, ref, computed, watch } from 'vue';
 import THInputText from '@/components/THInputText.vue';
 import MiniLangDropdown from '@/layout/Components/MiniLangDropdown.vue';
-import { Dialog } from '@headlessui/vue'
+//import { Dialog } from '@headlessui/vue'
 import { useGuestStore } from '@/stores/modules/guest'
 import { useLocaleStore } from '@/stores/modules/locale'
 import { useHotelStore } from '@/stores/modules/hotel'
