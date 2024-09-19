92bb620b86775f20dd6efbeab5c24fc00a369db7
jhjacoz96 <jhjacoz96@gmail.com>
2024-09-19 17:32:47 -0400

    git add .

--- src/api/services/experiencia.services.js
+++ src/api/services/experiencia.services.js
@@ -4,7 +4,7 @@ import {
 } from '../config/apiRoute'
 
 export const getAllApi = (params) => apiHttp('POST', `${experiencePath}/getAll`, params)
-export const getNumbersByFiltersApi = (params) => apiHttp('GET', `${experiencePath}/getNumbersByFilters`, params)
+export const getNumbersByFiltersApi = (params) => apiHttp('POST', `${experiencePath}/getNumbersByFilters`, params)
 export const findBySlugApi = (params) => apiHttp('GET', `${experiencePath}/findBySlug`, params)
 export const findInVIatorByShortIdApi = (params) => apiHttp('GET', `${experiencePath}/findInVIatorByShortId`, params)
 export const findSchedulesInVIatorApi = (params) => apiHttp('GET', `${experiencePath}/findSchedulesInVIator`, params)
