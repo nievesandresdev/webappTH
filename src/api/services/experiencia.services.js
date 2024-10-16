c323af6a27ea8f75b36def5f0b1e8b29eda25e5c
jhjacoz96 <jhjacoz96@gmail.com>
2024-10-16 12:35:37 -0400

    fix

--- src/api/services/experiencia.services.js
+++ src/api/services/experiencia.services.js
@@ -3,8 +3,8 @@ import {
     experiencePath,
 } from '../config/apiRoute'
 
-export const getAllApi = (params) => apiHttp('POST', `${experiencePath}/getAll`, params)
-export const getNumbersByFiltersApi = (params) => apiHttp('POST', `${experiencePath}/getNumbersByFilters`, params)
-export const findBySlugApi = (params) => apiHttp('GET', `${experiencePath}/findBySlug`, params)
-export const findInVIatorByShortIdApi = (params) => apiHttp('GET', `${experiencePath}/findInVIatorByShortId`, params)
-export const findSchedulesInVIatorApi = (params) => apiHttp('GET', `${experiencePath}/findSchedulesInVIator`, params)
+export const getAllApi = (params) => apiHttp('POST', `${experiencePath}/getAll`, params, {}, 'API_HELPER');
+export const getNumbersByFiltersApi = (params) => apiHttp('POST', `${experiencePath}/getNumbersByFilters`, params, {}, 'API_HELPER');
+export const findBySlugApi = (params) => apiHttp('GET', `${experiencePath}/findBySlug`, params, {}, 'API_HELPER');
+export const findInVIatorByShortIdApi = (params) => apiHttp('GET', `${experiencePath}/findInVIatorByShortId`, params, {}, 'API_HELPER');
+export const findSchedulesInVIatorApi = (params) => apiHttp('GET', `${experiencePath}/findSchedulesInVIator`, params, {}, 'API_HELPER');
