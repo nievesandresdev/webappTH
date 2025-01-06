ebab674c587e205b47e9feb9cc076baf7131faf3
Fran <francisco20990@gmail.com>
2025-01-06 13:07:52 -0400

    fix carrousel facilities

--- src/Modules/Hotel/ShowHotel.vue
+++ src/Modules/Hotel/ShowHotel.vue
@@ -61,33 +61,37 @@
         
         <HotelInfoGeneral :hotelData="hotelData" />
       </template>
-      <!--**************-->
+    </div>
+    <!-- seccion de instalaciones -->
+      <div class="flex flex-col  mt-1 sp:mt-2 px-2 sp:px-4">
+        <div class="border-t my-3 sp:my-6 border-[#E9E9E9]"></div>
 
-      <div class="border-t my-3 sp:my-6 border-[#E9E9E9]"></div>
-
-      <div class="flex items-center gap-2 sp:gap-4 mb-2 sp:mb-4" v-show="hotelData.show_facilities === 1" >
-        <p class="text-[10px] text-[16px] font-bold text-[#333333] lato">{{ $t('hotel.facilities') }}</p>
-        <div class="border-t border-[#E9E9E9] flex-grow ml-1 sp:ml-2"></div>
-        <span @click="goToFacilities()" class="underline lato text-[8px] sp:text-sm font-bold">{{ $t('hotel.utils.see_all') }}</span>
+        <div class="flex items-center gap-2 sp:gap-4 mb-2 sp:mb-4" v-show="hotelData.show_facilities === 1" >
+          <p class="text-[16px] font-bold text-[#333333] lato">{{ $t('hotel.facilities') }}</p>
+          <div class="border-t border-[#E9E9E9] flex-grow ml-1 sp:ml-2"></div>
+          <span @click="goToFacilities()" class="underline lato text-[8px] sp:text-sm font-bold">{{ $t('hotel.utils.see_all') }}</span>
+        </div>
       </div>
-
       <CardSlider 
           :data="facilities" 
           @itemClick="handleGoFacility" 
           v-show="hotelData.show_facilities === 1" 
           :marginBotton="rrss"  
-          :cover="false"
-        />
+          :cover="true"
+      />
+      <!-- fin seccion instalaciones -->
 
-      <template  v-if="rrss">
-        <div class="flex items-center gap-4 mb-4">
-          <p class="text-[16px] font-bold text-[#333333] lato">{{ $t('hotel.utils.our_networks') }}</p>
-          <div class="border-t border-[#E9E9E9] flex-grow ml-2"></div>
-        </div >
-        <HotelRRSS :hotelData="hotelData" />
-      </template>
-
-    </div>
+      <!-- Redes Sociales -->
+      <div class="flex flex-col mt-1 sp:mt-2 px-2 sp:px-4">
+          <template  v-if="rrss">
+          <div class="flex items-center gap-4 mb-4">
+            <p class="text-[16px] font-bold text-[#333333] lato">{{ $t('hotel.utils.our_networks') }}</p>
+            <div class="border-t border-[#E9E9E9] flex-grow ml-2"></div>
+          </div >
+          <HotelRRSS :hotelData="hotelData" />
+        </template>
+      </div>
+      <!-- fin redes sociales-->
   </div>
 
   <!-- Modal de Wifi -->
