d5fcfc8507503a5397b6e8506f27b955e8b7c0fe
xpestana <unibasileaweb@gmail.com>
2025-02-06 14:14:14 -0400

    Refactor Dockerfile and Nginx configuration for improved production setup and streamline build process

--- Dockerfile
+++ Dockerfile
@@ -1,28 +1,14 @@
-# Etapa de construcción
 FROM node:18-alpine as build-stage
-
-# Establece el directorio de trabajo
 WORKDIR /app
-
-# Copia archivos de dependencias antes del código
 COPY package*.json ./
-
-# Instala todas las dependencias, incluidas las de desarrollo (para vue-cli-service)
 RUN npm install
-
-# Copia el código fuente después de instalar las dependencias
 COPY . .
-
-# Construcción del proyecto Vue
 RUN npm run build
 
-# Etapa de producción (Nginx)
-FROM nginx:alpine as production-stage
-
-# Copiar la build generada en la etapa anterior
+# etapa de producción
+FROM nginx:1.13.12-alpine as production-stage
 COPY --from=build-stage /app/dist /usr/share/nginx/html
 
-# Copiar la configuración de Nginx
 COPY nginx.conf /etc/nginx/conf.d/default.conf
 
 EXPOSE 80
--- vue.config.js
+++ vue.config.js
@@ -2,22 +2,6 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-
-  // Optimización de Webpack para reducir el tamaño de los archivos
-  configureWebpack: {
-    optimization: {
-      splitChunks: {
-        chunks: "all",
-        maxSize: 500000, // Fragmenta archivos grandes en chunks menores de 500KB
-      }
-    },
-    externals: {
-      vue: "Vue",
-      "vue-router": "VueRouter",
-    }
-  },
-
-  // Configuración del Servidor de Desarrollo
   devServer: {
     host: '0.0.0.0',
     port: 81,
@@ -29,11 +13,11 @@ module.exports = defineConfig({
     //   webSocketURL: 'wss://localhost:81/ws', // Cambia `ws` a `wss`
     // },
     // disableHostCheck: true,
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true
-      }
-    }
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://127.0.0.1:8000',
+    //     changeOrigin: true
+    //   }
+    // }
   }
 });
