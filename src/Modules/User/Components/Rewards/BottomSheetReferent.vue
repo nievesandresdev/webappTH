9bf0e023b3a142867070eae196ef64a97a11acab
Fran <francisco20990@gmail.com>
2025-02-09 07:56:02 -0400

    fix shared referent msg

--- src/Modules/User/Components/Rewards/BottomSheetReferent.vue
+++ src/Modules/User/Components/Rewards/BottomSheetReferent.vue
@@ -99,8 +99,9 @@ const openModalShare = async () => {
     const data = {
         title: '¡Obtén un descuento especial!',
 
-        text: `Usa mi código de referido para obtener ${amountReferredFormat.value} de descuento en tu compra en ${dataShared.reward.url}.\n\nCódigo: _${dataShared.code}_\n\nPara canjearlo:\n\n${dataShared.reward.description}`,
+        text: `Usa mi código de referido para obtener ${amountReferredFormat.value} de descuento en tu compra en \n\n ${dataShared.reward.url}\n\nCódigo: _${dataShared.code}_\n\nPara canjearlo:\n\n${dataShared.reward.description}`,
         url : dataShared.full_url,
+
 
 
     };
