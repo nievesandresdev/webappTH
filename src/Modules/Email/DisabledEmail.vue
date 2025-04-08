c3e0523907250d8cdfaeed3d3888e2604e369d95
Fran <francisco20990@gmail.com>
2025-04-08 19:28:38 -0400

    fix

--- src/Modules/Email/DisabledEmail.vue
+++ src/Modules/Email/DisabledEmail.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="min-h-screen flex items-center justify-center bg-white text-center p-4">
-      <div class="max-w-md">
+      <div class="max-w-md" v-if="!enabledEmail">
         <h1 class="text-[16px] text-[#333] mb-4 roboto font-medium ">
           Te has dado de baja con éxito. Ya no recibirás estos correos.
         </h1>
         <p class="text-[#333] font-medium text-[16px] roboto">
           ¿Ha sido un error? Haz
-          <a @click="reactivateEmail" class="text-[#333] underline">
+          <a @click="reactivateEmail" class="text-[#333] underline cursor-pointer">
              clic aquí
           </a>
           &nbsp;para volver a activarlos.
         </p>
       </div>
-      <div class="max-w-md" v-if="enabledEmail">
+      <div class="max-w-md" v-else>
         <h1 class="text-[16px] text-[#333] mb-4 roboto font-medium ">
           Se ha reactivado tu suscripción
         </h1>
