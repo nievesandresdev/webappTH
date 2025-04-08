fadfc0a6cf48a329b5d08ae8c4338c86c8c6646d
Fran <francisco20990@gmail.com>
2025-04-08 15:16:03 -0400

    fix

--- src/Modules/Email/DisabledEmail.vue
+++ src/Modules/Email/DisabledEmail.vue
@@ -1,14 +1,15 @@
 <template>
     <div class="min-h-screen flex items-center justify-center bg-white text-center p-4">
       <div class="max-w-md">
-        <h1 class="text-lg font-semibold text-black mb-4">
+        <h1 class="text-[16px] text-[#333] mb-4 roboto font-medium ">
           Te has dado de baja con éxito. Ya no recibirás estos correos.
         </h1>
-        <p class="text-black">
-          ¿Ha sido un error?
-          <a href="/reactivar-correos" class="text-blue-600 underline">
-            Haz clic aquí para volver a activarlos.
+        <p class="text-[#333] font-medium text-[16px] roboto">
+          ¿Ha sido un error? Haz
+          <a @click="reactivateEmail" class="text-[#333] underline">
+             clic aquí
           </a>
+          &nbsp;para volver a activarlos.
         </p>
       </div>
     </div>
@@ -24,5 +25,9 @@
     const gParam = route.query.g
     console.log('Código recibido (g):', gParam)
   })
+
+  const reactivateEmail = () => {
+    console.log('Reactivando correo...')
+  }
   </script>
   
