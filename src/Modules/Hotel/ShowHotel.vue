c5f8f81b81dc8fd9b87484505194a3b4dd8be202
Fran <francisco20990@gmail.com>
2025-04-22 09:21:53 -0400

    fix

--- src/Modules/Hotel/ShowHotel.vue
+++ src/Modules/Hotel/ShowHotel.vue
@@ -25,7 +25,6 @@
                 :class="isExpanded ? 'text-[10px] sp:text-sm font-normal lato text-[#333] mt-2 sp:mt-3' : 'text-[10px] sp:text-sm font-normal lato text-[#333] truncate-description mt-2 sp:mt-3'"
               >
                 {{localeStore.localeCurrent == 'es' ? hotelInfo?.description : hotelInfo?.translate?.description}}
-                <pre>{{ hotelInfo?.translate }}</pre>
               </p>
 
               <p
