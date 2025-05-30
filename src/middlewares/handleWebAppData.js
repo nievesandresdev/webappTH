c6c1689a490012934926650e3df48c9faa52cce7
Andres <andresdreamerf@gmail.com>
2025-05-30 12:04:30 -0400

    fix session

--- src/middlewares/handleWebAppData.js
+++ src/middlewares/handleWebAppData.js
@@ -97,7 +97,6 @@ export default async function handleWebAppData({ to, from, next }) {
     if(guestId){
         if (googleId) {
             await authStore.$loginByGoogle(guestId, googleId);
-            return next({ name: 'Home', params :{ hotelSlug: chainStore.chainData?.independentSubdomain}, query: to.query }); // quitar query googleId
         } else {
             let localGuest = guestStore.getLocalGuest();
             if(!localGuest || localGuest && Number(localGuest.id) !== Number(guestId)){
