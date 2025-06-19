a92cc40e070e9530dcba54703d3f739303fde22e
Fran <francisco20990@gmail.com>
2025-06-19 14:36:47 -0400

    fix

--- src/Modules/Home/HomePageRed.vue
+++ src/Modules/Home/HomePageRed.vue
@@ -171,7 +171,7 @@ const placeCategories = ref(null)
 const catWhatVisitId = ref(null)
 const catWhereEatId = ref(null)
 const catLeisureId  = ref(null)
-const showTutorial = ref(true)
+const showTutorial = ref(false)
 
 startLoading(SECTIONS.HOME.GLOBAL);
 
