6d5d03ce52bbb145b9f88a776d78c0bf47b5672a
Fran <francisco20990@gmail.com>
2025-07-02 14:54:13 -0400

    fix merge

--- src/Modules/Home/Components/RecomendationPlaceSections.vue
+++ src/Modules/Home/Components/RecomendationPlaceSections.vue
@@ -1,21 +1,4 @@
 <template>
-<<<<<<< HEAD
-    <div class="flex items-center gap-2 px-3 py-2 ml-1 mb-2">
-        <img src="/assets/icons/WA.STAR.BLACK.svg" class="w-6 h-6" alt="star">
-        <h2 class="lato text-[20px] font-bold">
-            {{ $t('home.recomendation-places.title') }}
-        </h2>
-    </div>
-    <FeaturedPlacesSlider 
-        v-if="orderSections?.placesRecommendationSection?.style === 2"
-        :places="allPlaces"
-    />
-
-    <FeaturedPlacesDotsSlider 
-        v-if="orderSections?.placesRecommendationSection?.style === 1"
-        :places="allPlaces"
-    />
-=======
     <div class="my-1 sp:my-2">
         <div class="flex items-center gap-2 px-3 ml-1 mb-2">
             <img src="/assets/icons/WA.STAR.BLACK.svg" class="w-6 h-6" alt="star">
@@ -24,17 +7,16 @@
             </h2>
         </div>
         <FeaturedPlacesSlider 
-            v-if="orderSections?.placesRecommendationSection?.style === 1"
+            v-if="orderSections?.placesRecommendationSection?.style === 2"
             :places="allPlaces"
         />
 
         <FeaturedPlacesDotsSlider 
-            v-if="orderSections?.placesRecommendationSection?.style === 2"
+            v-if="orderSections?.placesRecommendationSection?.style === 1"
             :places="allPlaces"
         />
     </div>
     
->>>>>>> 80b0601586565ab2128d78295d18f67ba8424222
 </template>
 <script setup>
 import FeaturedPlacesSlider from './FeaturedPlacesSlider.vue';
