031436afd1fb1221e59363d3c6d5e3a4b9f459d8
jhjacoz96 <jhjacoz96@gmail.com>
2025-07-02 14:44:41 -0400

    fix

--- src/Modules/Home/Components/SectionServices.vue
+++ src/Modules/Home/Components/SectionServices.vue
@@ -4,27 +4,27 @@
         class="section-services"
         @click="goToService('servicios/confort')"
     >
-        <div class="flex space-x-[6px] sp:space-x-[8px] mb-[6px] sp:mb-[8px]">
+        <div class="flex items-center space-x-[6px] sp:space-x-[8px] mb-[6px] sp:mb-[8px]">
             <img src="/assets/icons/WA.MENU.DEFAULT.SERVICE.svg" alt="services" class="size-[16px] sp:size-[24px]">
-            <h2 class="font-[20px] font-bold lato">{{ $t('service.title') }}</h2>
+            <h2 class="text-[11px] sp:text-[20px] font-bold lato">{{ $t('service.title') }}</h2>
         </div>
         <div class="flex space-x-[6px] sp:space-x-[8px] items-center px-[6px] sp:px-[8px] pt-[6px] sp:pt-[8px] pb-[12px] sp:pb-[16px]">
             <div class="flex-1">
-                <p class="text-[12px] sp:text-[16px] font-bold lato mb-[6px] sp:mb-[8px] leading-[20px]">{{ $t('home.section-services.text-on') }}</p>
-                <p class="text-[8px] sp:text-[12px] lato mb-[12px] sp:mb-[16px] text-[#777777] leading-[14px]">
+                <p class="text-[10px] sp:text-[16px] font-bold lato mb-[6px] sp:mb-[8px] leading-[12px] sp:leading-[20px]">{{ $t('home.section-services.text-on') }}</p>
+                <p class="text-[8px] sp:text-[12px] lato mb-[12px] sp:mb-[16px] text-[#777777] leading-[7px] sp:leading-[14px]">
                     {{ $t('home.section-services.text-off') }}
                 </p>
                 <button
-                    class="space-x-[6px] sp:space-x-[8px] flex items-center font-bold lato"
+                    class="space-x-[6px] sp:space-x-[8px] flex items-center"
                     @click="goToService('servicios/confort')"
                 >
-                    <span>
+                    <span class="text-[10px] sp:text-[14px] font-bold lato">
                         {{ $t('home.section-services.btn-discover') }}
                     </span>
                     <img src="/assets/icons/arrow-right-long.svg" alt="arrow" class="size-[12px] sp:size-[16px]">
                 </button>
             </div>
-            <div class="w-[80px] sp:w-[115px]">
+            <div class="w-[60px] sp:w-[115px]">
                 <img src="/assets/img/card-services.png" alt="card-services" class="w-full">
             </div>
         </div>
@@ -34,41 +34,41 @@
         v-else-if="orderSections?.servicesSection?.style == 2"
         class="section-services"
     >
-        <div class="flex space-x-2 mb-2">
-            <img src="/assets/icons/WA.MENU.DEFAULT.SERVICE.svg" alt="services" class="size-[24px]">
-            <h2 class="font-[20px] font-bold lato">{{ $t('service.title') }}</h2>
+        <div class="flex items-center space-x-[6px] sp:space-x-[8px] mb-[6px] sp:mb-[8px]">
+            <img src="/assets/icons/WA.MENU.DEFAULT.SERVICE.svg" alt="services" class="size-[16px] sp:size-[24px]">
+            <h2 class="text-[11px] sp:text-[20px] font-bold lato">{{ $t('service.title') }}</h2>
         </div>
-        <div class="pt-[8px] pb-[16px]">
-            <div class="grid gap-x-2" :class="`grid-cols-${services.length}`">
+        <div class="pt-[6px] sp:pt-[8px] pb-[12px] sp:pb-[16px]">
+            <div class="grid gap-x-[6px] sp:gap-x-[8px]" :class="`grid-cols-${services.length}`">
                 <div
                     v-for="(service, index) in services"
                     :key="service.name"
-                    class="h-[120px] bg-[#000000] flex items-end justify-center pb-[8px]"
-                    :class="`rounded-${getDirectionRounded(index)}-[10px]`"
+                    class="h-[90px] sp:h-[140px] bg-[#000000] flex items-end justify-center pb-[6px] sp:pb-[8px]"
+                    :class="`rounded-${getDirectionRounded(index)}-[7px] sp:rounded-${getDirectionRounded(index)}-[10px]`"
                     :style="{ backgroundImage: `url(${service.pathImg})`, backgroundSize: 'cover', backgroundPosition: 'center', background: `linear-gradient(0deg, rgba(0, 0, 0, 0.30) 0%, rgba(0, 0, 0, 0.30) 100%), url(${service.pathImg}) lightgray ${calDimentionImg()}` }"
                     @click="goToService(service.to)"
                 >
                     <div class="flex flex-col items-center">
-                        <img :src="service.icon" :alt="service.name" class="size-[48px] z-10">
-                        <span class="text-[18px] font-bold lato text-white mt-[8px]">{{ service.name }}</span>
+                        <img :src="service.icon" :alt="service.name" class="size-[32px] sp:size-[48px] z-10">
+                        <span class="text-[12px] sp:text-[18px] font-bold lato text-white mt-[6px] sp:mt-[8px]">{{ service.name }}</span>
                     </div>
                 </div>
             </div>
-            <div class="px-[16px] mt-[16px]">
-                <p class="text-[16px] font-bold lato mb-[8px] leading-[20px]">{{ $t('home.section-services.text-on') }}</p>
-                <p class="text-[14px] lato mb-[16px] text-[#777777] mb-[16px] leading-[14px]">
+            <div class="px-[10px] sp:px-[16px] mt-[10px] sp:mt-[16px]">
+                <p class="text-[12px] sp:text-[16px] font-bold lato mb-[6px] sp:mb-[8px] leading-[12px] sp:leading-[20px]">{{ $t('home.section-services.text-on') }}</p>
+                <p class="text-[10px] sp:text-[14px] lato mb-[12px] sp:mb-[16px] text-[#777777] leading-[10px] sp:leading-[14px]">
                     {{ $t('home.section-services.text-off') }}
                 </p>
             </div>
-            <div class="px-[16px] flex justify-end">
+            <div class="px-[10px] sp:px-[16px] flex justify-end">
                 <button
-                    class="space-x-2 flex items-center font-bold lato"
+                    class="space-x-[6px] sp:space-x-[8px] flex items-center"
                     @click="goToService('servicios/confort')"
                 >
-                    <span>
+                    <span class="text-[10px] sp:text-[14px] font-bold lato">
                         {{ $t('home.section-services.btn-discover') }}
                     </span>
-                    <img src="/assets/icons/arrow-right-long.svg" alt="arrow" class="size-[16px]">
+                    <img src="/assets/icons/arrow-right-long.svg" alt="arrow" class="size-[12px] sp:size-[16px]">
                 </button>
             </div>
         </div>
@@ -152,9 +152,16 @@ const goToService = (to) => {
 
 .section-services {
     background: #FAFAFA;
-    border-radius: 10px;
+    border-radius: 7px;
     box-shadow: 0px 3px 8px 0px rgba(0, 0, 0, 0.12), 0px 3px 1px 0px rgba(0, 0, 0, 0.04);
-    margin: 0 16px;
+    margin: 0 10px;
+}
+
+@media (min-width: 300px) {
+    .section-services {
+        border-radius: 10px;
+        margin: 0 16px;
+    }
 }
     
 </style>
