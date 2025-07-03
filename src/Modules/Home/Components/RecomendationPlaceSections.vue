d2a1c0c8b1165ca9fdb6e13f731a1012e7342301
Fran <francisco20990@gmail.com>
2025-07-03 13:45:28 -0400

    fix

--- src/Modules/Home/Components/RecomendationPlaceSections.vue
+++ src/Modules/Home/Components/RecomendationPlaceSections.vue
@@ -2,7 +2,7 @@
     <div class="my-1 sp:my-2">
         <div class="flex items-center gap-2 px-3 ml-1 mb-2">
             <img src="/assets/icons/WA.STAR.BLACK.svg" class="sp:w-6 sp:h-6 w-4 h-4" alt="star">
-            <h2 class="lato sp:text-[20px] text-[15px] font-bold">
+            <h2 class="lato sp:text-[20px] text-[13px] font-bold">
                 {{ $t('home.recomendation-places.title') }}
             </h2>
         </div>
